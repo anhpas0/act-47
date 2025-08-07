d3978cb8d5bb7220dc791b214d7da2a6006672ee
Anh Nguyen <anhpas0@gmail.com>
2025-08-07 21:43:45 +0700

    Update file cf

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Cấu hình này sẽ bảo Next.js bỏ qua các lỗi từ ESLint (bao gồm cả các quy tắc của TypeScript)
+  // trong quá trình build cho môi trường production.
+  eslint: {
+    // Cảnh báo: Việc bật tùy chọn này sẽ cho phép ứng dụng của bạn build thành công
+    // ngay cả khi nó có các lỗi ESLint. Điều này hữu ích để deploy nhanh,
+    // nhưng bạn nên quay lại sửa các lỗi đó sau để đảm bảo chất lượng code.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
